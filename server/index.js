f41ed0de01f4321855a831b6b97465d7767a3f81
Aaditya Mishra <codewithaadi31@gmail.com>
2023-01-06 03:28:52 +0530

    Backend: Create Operation is completed.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-import { Express } from "express";
+import express  from "express";
+import dotenv from 'dotenv';
+import Connection from "./database/db.js";
+import Routes from './routes/route.js'
+import cors from 'cors';
+import bodyParser from "body-parser";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use('/',Routes);
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const PORT= 8000;
+
+
+Connection(username,password);
+
+app.listen(PORT,()=> console.log(`!!! Server is running succesfully on ${PORT}`));
